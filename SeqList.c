dfd9218e365d1bcbedcdd35341b7705050d3da54
王靖凯 <wjk930726@live.com>
2018-04-15 17:43:12 +0800

    <<<<<<<<<<<<<<< HEAD

--- SeqList.c
+++ SeqList.c
@@ -3,3 +3,23 @@
 //
 
 #include "SeqList.h"
+
+SeqList *SeqList_Create(int capacity) { return NULL; }
+
+void SeqList_Destroy(SeqList *list) {}
+
+void SeqList_Clear(SeqList *list) {}
+
+int SeqList_Length(SeqList *list) { return 0; }
+
+int SeqList_Capacity(SeqList *list) { return 0; }
+
+int SeqList_Insert(SeqList *list, SeqListNode *node, int pos) {
+    for (int i = SeqList_Length(list); i < pos; --i) {
+
+    }
+    return 0; }
+
+SeqListNode *SeqList_Get(SeqList *list, int pos) { return NULL; }
+
+SeqListNode *SeqList_Delete(SeqList *list, int pos) { return NULL; }
--- main.c
+++ main.c
@@ -4,11 +4,50 @@
 #include "LinkedList.h"
 #include "SeqList.h"
 
+typedef struct {
+    Node *next;
+    int data;
+} Data;
+
 int test_linked_list();
+int test_seq_lise();
 
 int main() {
-    int res = test_linked_list();
+    int res = -1;
+    //    res = test_linked_list();
+    res = test_seq_lise();
     return res;
+}
+
+int test_seq_lise() {
+    SeqList *list = NULL;
+
+    Data d1, d2, d3;
+    d1.data = 1;
+    d2.data = 2;
+    d3.data = 3;
+
+    list = SeqList_Create(10);
+    int length = SeqList_Length(list);
+    SeqList_Insert(list, (Node *)&d1, length);
+    SeqList_Insert(list, (Node *)&d2, length);
+    SeqList_Insert(list, (Node *)&d3, length);
+    for (int i = 0; i < SeqList_Length(list); ++i) {
+        Data *data = SeqList_Get(list, i);
+        if (data != NULL) {
+            printf("data is %d\n", data->data);
+        }
+    }
+
+    while (SeqList_Length(list) > 0) {
+        Data *data = SeqList_Delete(list, 0);
+        if (data != NULL) {
+            printf("data is %d\n", data->data);
+        }
+    }
+
+    SeqList_Destroy(list);
+    return 0;
 }
 
 int test_linked_list() {
