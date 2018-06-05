1631fcc9a5f84dd5646b74704dd3550212bbc2bc
王靖凯 <wjk930726@live.com>
2018-06-05 23:32:50 +0800

    --help

--- Sort.c
+++ Sort.c
@@ -33,38 +33,15 @@ void swap(int array[], int i, int j) {
     array[j] = temp;
 }
 
-void InertionSort_ddddd(int array[], int len) // O(n*n)
-{
-    int i    = 0;
-    int j    = 0;
-    int k    = -1;
-    int temp = -1;
-
-    //{12, 5, 433, 253, 216, 7};
-    for (i = 1; i < len; i++) {
-        k    = i; //待插入位置
-        temp = array[k];
-
-        for (j = i - 1; (j >= 0) && (array[j] > temp); j--) {
-            array[j + 1] = array[j]; //元素后移
-            k            = j;        // k需要插入的位置
-        }
-
-        array[k] = temp; //元素插入
-    }
-}
-
-// nlogn
-void ShellSort(int array[], int len) //
-{
+void ShellSort(int array[], int len) {
     int i    = 0;
     int j    = 0;
     int k    = -1;
     int temp = -1;
     int gap  = len;
     do {
-        //业界统一实验的 平均最好情况 经过若干次后，收敛为1
-        gap = gap / 3 + 1; // gap /2345 2000 都行  //O（n 1.3）
+
+        gap = gap / 3 + 1;
 
         for (i = gap; i < len; i += gap) {
             k    = i;
@@ -81,13 +58,41 @@ void ShellSort(int array[], int len) //
     } while (gap > 1);
 }
 
-int main() {
-    int array[] = { 12, 5, 433, 253, 216, 7 };
-    int len     = sizeof(array) / sizeof(*array);
+int partition(int array[], int low, int high) {
+    int pv = array[low];
 
-    println(array, len);
-    ShellSort(array, len);
-    println(array, len);
-    system("pause");
-    return 0;
+    while (low < high) {
+        while ((low < high) && (array[high] >= pv)) {
+            high--;
+        }
+        swap(array, low, high);
+        while ((low < high) && (array[low] <= pv)) {
+            low++;
+        }
+        swap(array, low, high);
+    }
+
+    return low;
+}
+
+void QSort2(int array[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(array, low, high);
+
+        QSort2(array, low, pivot - 1);
+        QSort2(array, pivot + 1, high);
+    }
+}
+
+void QSort(int array[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(array, low, high);
+
+        QSort2(array, low, pivot - 1);
+        QSort2(array, pivot + 1, high);
+    }
+}
+
+void QuickSort(int array[], int len) {
+    QSort(array, 0, len - 1);
 }
