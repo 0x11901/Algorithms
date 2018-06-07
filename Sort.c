b7a47a6a2fc45aaf05f5b10e5a191e7a90d444f8
王靖凯 <wjk930726@live.com>
2018-06-07 23:55:34 +0800

    I was wrong...

--- Sort.c
+++ Sort.c
@@ -96,3 +96,48 @@ void QSort(int array[], int low, int high) {
 void QuickSort(int array[], int len) {
     QSort(array, 0, len - 1);
 }
+
+void Merge(const int src[], int des[], int low, int mid, int high) {
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    while ((i <= mid) && (j <= high)) {
+        if (src[i] < src[j]) {
+            des[k++] = src[i++];
+        }
+        else {
+            des[k++] = src[j++];
+        }
+    }
+
+    while (i <= mid) {
+        des[k++] = src[i++];
+    }
+
+    while (j <= high) {
+        des[k++] = src[j++];
+    }
+}
+
+void MSort(int src[], int des[], int low, int high, int max) {
+    if (low == high) {
+        des[low] = src[low];
+    }
+    else {
+        int  mid   = (low + high) / 2;
+        int *space = (int *)malloc(sizeof(int) * max);
+
+        if (space != NULL) {
+            MSort(src, space, low, mid, max);
+            MSort(src, space, mid + 1, high, max);
+            Merge(space, des, low, mid, high);
+        }
+
+        free(space);
+    }
+}
+
+void MergeSort(int array[], int len) {
+    MSort(array, array, 0, len - 1, len);
+}
