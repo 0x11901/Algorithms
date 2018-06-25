250b7ed06c2071d312c288661987b824ec665d91
王靖凯 <wjk930726@live.com>
2018-06-25 22:53:29 +0800

    last minute fixes.

--- Functional/src/Boolean.hs
+++ Functional/src/Boolean.hs
@@ -11,3 +11,27 @@ not True = False
 not False = True
 not _ = False
 
+xor, and, or :: Bool -> Bool -> Bool
+xor b1 b2 = not (b1 == b2)
+
+and True b1 = b1
+and False _ = False
+
+or False b1 = b1
+or True _ = True
+
+condition :: Bool -> a -> a -> a
+condition True t f = t
+condition False t f = f
+
+infix 4 ==
+infix 4 /=
+infix 3 &&
+infix 3 ||
+
+(||) = or
+(&&) = and
+(/=) = not
+
+hA :: Bool -> Bool -> (Bool, Bool)
+hA a b = (a /= b, a && b)
--- Functional/src/Main.hs
+++ Functional/src/Main.hs
@@ -3,4 +3,3 @@ module Main where
 main :: IO ()
 main = do
     putStrLn "hello world!"
-    (\x -> \y -> x y) abs (-5)
