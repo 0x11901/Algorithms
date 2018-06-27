bf5ccc61de4346bda3d00a80ea809827d892303e
王靖凯 <wjk930726@live.com>
2018-06-27 23:38:24 +0800

    I'll explain when you're older!

--- Functional/src/Main.hs
+++ Functional/src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 main :: IO ()
 main = putStrLn "hello world!"
 
+
 power :: Int -> Int -> Int
 power 0 0 = 1
 power _ 0 = 1
@@ -13,3 +14,4 @@ power' 0 0 = 1
 power' _ 0 = 1
 power' x n | odd n = let p = power' x ((n - 1) `div` 2) in x * p * p
            | otherwise = let p = power' x (n `div` 2) in p * p
+           
