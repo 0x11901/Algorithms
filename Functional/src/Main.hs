c971647ffc12fdbdcc1ea50ce6ef0031dc314b9e
王靖凯 <wjk930726@live.com>
2018-06-30 18:01:06 +0800

    Lock S-foils in attack position

--- Functional/src/Main.hs
+++ Functional/src/Main.hs
@@ -28,5 +28,30 @@ mygcd x y = if y == 0 then x else mygcd y (mod x y)
 product' []       = 1
 product' (x : xs) = x * product' xs
 
-total []       = 0
-total (x : xs) = x + total xs
+-- total []       = 0
+-- total (x : xs) = x + total xs
+
+total' []       n = n
+total' (x : xs) n = total' xs $! (x + n)
+total xs = total' xs 0
+
+mc n | n > 100   = n - 10
+     | otherwise = mc (mc (n + 11))
+
+fibonacci :: (Num a, Eq a) => a -> a
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+
+fibs n = map fibonacci [0 .. n]
+
+fibStep :: Num a => (a, a) -> (a, a)
+fibStep (u, v) = (v, u + v)
+
+fibPair :: (Eq a, Num a) => a -> (a, a)
+fibPair 0 = (0, 1)
+fibPair n = fibStep (fibPair (n - 1))
+
+fastFib :: (Eq b, Num b) => b -> b
+fastFib n = fst $ fibPair n
+fibs' n = map fastFib [0 .. n]
