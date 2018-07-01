a5e84b3d08d506ed4b7308665507c27237405dda
王靖凯 <wjk930726@live.com>
2018-07-01 22:03:00 +0800

    fix tpyo

--- Functional/src/Rome.hs
+++ Functional/src/Rome.hs
@@ -1,3 +1,6 @@
+import           Data.List
+import           Data.Maybe
+
 romeNotation :: [String]
 romeNotation =
     ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
@@ -14,3 +17,20 @@ subtrahend n = head (dropWhile (\(a, _) -> a > n) pair)
 convert :: Int -> String
 convert 0 = ""
 convert n = let (rome, m) = subtrahend n in m ++ convert (n - rome)
+
+--   convert 17
+-- = "X" ++ convert (17 - 10)
+-- = "X" ++ "V" ++ convert (7 - 5)
+-- = "X" ++ "V" ++ "I" convert (2 - 1)
+-- = "X" ++ "V" ++ "I" ++ "I" convert (1 - 1)
+-- = "X" ++ "V" ++ "I" ++ "I" ++ ""
+-- = "XVII"
+
+subtrahend' :: String -> (Int, String)
+subtrahend' n = head (dropWhile (\(_, a) -> not (a `isPrefixOf` n)) pair)
+
+convert' :: String -> Int
+convert' [] = 0
+convert' n =
+    let (rome, m) = subtrahend' n
+    in  rome + convert' (fromMaybe "" (stripPrefix m n))
