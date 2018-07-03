2996e99161699876e006349ebc77b80f58e0814b
王靖凯 <wjk930726@live.com>
2018-07-03 23:24:59 +0800

    My bad

--- Functional/src/Main.hs
+++ Functional/src/Main.hs
@@ -1,4 +1,6 @@
 module Main where
+import           Data.List
+import           Data.Maybe
 
 main :: IO ()
 main = putStrLn "no head feelings"
@@ -69,3 +71,55 @@ fibPairs n = map fibPair [1 .. n]
 
 difference :: Int -> [Int]
 difference n = map (\(f1, f2, f3) -> f1 * f3 - f2 * f2) (combine $ fibPairs n)
+
+
+romeNotation :: [String]
+romeNotation =
+    ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+romeAmount :: [Int]
+romeAmount = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+pair :: [(Int, String)]
+pair = zip romeAmount romeNotation
+
+subtrahend :: Int -> (Int, String)
+subtrahend n = head (dropWhile (\(a, _) -> a > n) pair)
+
+convert :: Int -> String
+convert 0 = ""
+convert n = let (rome, m) = subtrahend n in m ++ convert (n - rome)
+
+--   convert 17
+-- = "X" ++ convert (17 - 10)
+-- = "X" ++ "V" ++ convert (7 - 5)
+-- = "X" ++ "V" ++ "I" convert (2 - 1)
+-- = "X" ++ "V" ++ "I" ++ "I" convert (1 - 1)
+-- = "X" ++ "V" ++ "I" ++ "I" ++ ""
+-- = "XVII"
+
+subtrahend' :: String -> (Int, String)
+subtrahend' n = head (dropWhile (\(_, a) -> not (a `isPrefixOf` n)) pair)
+
+convert' :: String -> Int
+convert' [] = 0
+convert' n =
+    let (rome, m) = subtrahend' n
+    in  rome + convert' (fromMaybe "" (stripPrefix m n))
+
+search :: (Ord a) => a -> [a] -> Bool
+search a [] = False
+search a xs | m < a     = search a behind
+            | m > a     = search a front
+            | otherwise = True
+    where (front, m : behind) = splitAt (length xs `div` 2) xs
+
+-- fixme: append all element to one list
+search' :: (Ord a) => a -> [a] -> [a]
+search' a [] = []
+search' a xs | m < a     = search' a behind
+             | m > a     = search' a front
+             | otherwise = [a]
+    where (front, m : behind) = splitAt (length xs `div` 2) xs
+
+
