20fdf622736f03a7a84aae349103cdea6648f686
王靖凯 <wjk930726@live.com>
2018-07-08 22:06:32 +0800

    IEize

--- Functional/src/Main.hs
+++ Functional/src/Main.hs
@@ -122,4 +122,85 @@ search' a xs | m < a     = search' a behind
              | otherwise = [a]
     where (front, m : behind) = splitAt (length xs `div` 2) xs
 
+insert' :: Ord a => a -> [a] -> [a]
+insert' x [] = [x]
+insert' x (y : ys) | x < y     = x : y : ys
+                   | otherwise = y : insert' x ys
 
+insertionSort :: Ord a => [a] -> [a] -> [a]
+insertionSort xs []       = xs
+insertionSort xs (y : ys) = insertionSort (insert' y xs) ys
+
+swaps :: Ord a => [a] -> [a]
+swaps []  = []
+swaps [x] = [x]
+swaps (x1 : x2 : xs) | x1 > x2   = x2 : swaps (x1 : xs)
+                     | otherwise = x1 : swaps (x2 : xs)
+
+fix :: Eq a => (a -> a) -> a -> a
+fix f x = if x == x' then x else fix f x' where x' = f x
+
+bubbleSort :: Ord a => [a] -> [a]
+bubbleSort = fix swaps
+
+bubbleSort' :: Ord a => [a] -> [a]
+bubbleSort' xs | swaps xs == xs = xs
+               | otherwise      = bubbleSort' $ swaps xs
+
+bubbleSort'' :: Ord a => [a] -> [a]
+bubbleSort'' [] = []
+bubbleSort'' xs = bubbleSort'' initialElements ++ [lastElement]
+  where
+    swappedxs       = swaps xs
+    initialElements = init swappedxs
+    lastElement     = last swappedxs
+
+delete' :: Eq a => a -> [a] -> [a]
+delete' _ [] = []
+delete' x (l : ls) | x == l    = ls
+                   | otherwise = l : delete' x ls
+
+selectionSort [] = []
+selectionSort xs = mini : selectionSort xs'
+  where
+    mini = minimum xs
+    xs'  = delete mini xs
+
+quickSort :: Ord a => [a] -> [a]
+quickSort []       = []
+quickSort (x : xs) = quickSort mini ++ [x] ++ quickSort maxi
+  where
+    mini = filter (< x) xs
+    maxi = filter (>= x) xs
+
+filterSplit :: (a -> Bool) -> [a] -> ([a], [a])
+filterSplit _ [] = ([], [])
+filterSplit f (x : xs) | f x       = (x : l, r)
+                       | otherwise = (l, x : r)
+    where (l, r) = filterSplit f xs
+
+quickSort' :: Ord a => [a] -> [a]
+quickSort' []       = []
+quickSort' [x     ] = [x]
+quickSort' (x : xs) = quickSort' l ++ [x] ++ quickSort' r
+    where (l, r) = filterSplit (< x) xs
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x : xs) (y : ys) | x > y     = y : merge (x : xs) ys
+                        | otherwise = x : merge xs (y : ys)
+
+mergeSort :: Ord a => [a] -> [a]
+mergeSort []  = []
+mergeSort [x] = [x]
+mergeSort xs  = merge (mergeSort x1) (mergeSort x2)
+  where
+    (x1, x2) = halve xs
+    halve xs = (take l xs, drop l xs)
+    l = length xs `div` 2
+
+halt :: Integral a => a -> [a]
+halt 1 = [1]
+halt n | even n    = let n' = div n 2 in n' : halt n'
+       | otherwise = let n' = 3 * n + 1 in n' : halt n'
