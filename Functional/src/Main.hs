7ef7230c65171f98897ac9a602a6d5f711792f14
王靖凯 <wjk930726@live.com>
2018-07-17 00:05:04 +0800

    Updated

--- Functional/src/Main.hs
+++ Functional/src/Main.hs
@@ -271,13 +271,14 @@ noSameDiag xs@(x : xs') = and [ abs (i1 - i) /= abs (p1 - p) | (i, p) <- ip ]
     && noSameDiag xs'
     where (i1, p1) : ip = zip [1 ..] xs
 
-queen n = [xs| xs <- positions n n, noSameRow xs, noSameDiag xs]
+queen n = [ xs | xs <- positions n n, noSameRow xs, noSameDiag xs ]
 
 positions' 0 n = [[]]
-positions' k n = [p:ps|ps<- positions' (k - 1 ) n, p <- [1 .. n] , isSafe p ps]
+positions' k n =
+    [ p : ps | ps <- positions' (k - 1) n, p <- [1 .. n], isSafe p ps ]
 
-isSafe p ps = not (elem p ps|| sameDiag p ps)
-    where
-         sameDiag p ps = any (\(dist, q) -> abs (p - q) == dist) $ zip [1..] ps
+isSafe p ps = not (elem p ps || sameDiag p ps)
+  where
+    sameDiag p ps = any (\(dist, q) -> abs (p - q) == dist) $ zip [1 ..] ps
 
 queens = positions' 8 8
