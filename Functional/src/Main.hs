831ebcd0a8ab01833b039492d511d7c27f872292
王靖凯 <wjk930726@live.com>
2018-07-22 14:58:01 +0800

    Whee, good night.

--- Functional/src/Main.hs
+++ Functional/src/Main.hs
@@ -293,3 +293,13 @@ permutation (x : xs) = concat [ insert'' x permux | permux <- permutation xs ]
 
 queens' :: Int -> [[Int]]
 queens' n = [xs| xs <- permutations [1..n], noSameDiag xs]
+
+infixl 5 |*|
+(|*|) :: Num a => [[a]] -> [[a]] -> [[a]]
+(|*|) a b = [[ sum $ zipWith (*) ar bc | bc <- transpose b ] | ar <- a]
+
+unit = [[1,1],[1,0]]
+
+fib 1 = unit
+fib n | even n = let m = fib (div n 2) in m |*| m
+      | otherwise = let m = fib (div (n-1) 2) in m |*| unit |*| m
