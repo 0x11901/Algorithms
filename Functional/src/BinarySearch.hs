6d369617620af70f7c5ea61c5956e975c3d2ed99
0x11901 <wjk930726@live.com>
2019-02-13 14:15:45 +0800

    tagging release w.t.f.

--- Functional/src/BinarySearch.hs
+++
@@ -1,6 +0,0 @@
-search :: (Ord a) => a -> [a] -> Bool
-search a [] = False
-search a xs | m < a     = search a behind
-            | m > a     = search a front
-            | otherwise = True
-    where (front, m : behind) = splitAt (length xs `div` 2) xs
